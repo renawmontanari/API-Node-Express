824c731f3a677b9fb4e84a03dbd33ae5b572f8b6
renawmontanari <regiangelica@gmail.com>
2023-10-31 23:46:06 -0300

    API-Node-Express

--- API/status_na_resposta/index.js
+++ API/status_na_resposta/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
-
-app.use(express.json)
+app.use(express.json())
 
 // Rotas - Endpoints
-app.get('/', (req, res) => {
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price 
 
-    res.status(201).json({message: 'Primeira rota criada com sucesso!'})
+    if(!name) {
+        res.status(422).json({ message: 'O campo nome é obrigatorio' })
+        return
+    }
+
+    console.log(name)
+    console.log(price)
+
+    res.status(201).json({ message: `O produto ${name} foi criado com sucesso!` })
 
 })
 
